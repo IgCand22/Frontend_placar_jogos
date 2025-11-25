4c351bde6143c63b34a3acf6c89df810ea694766
Arthur <arthur.wagner.lemos@gmail.com>
2025-11-25 19:01:53 -0300

    Excluido div descrição

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const placar_title = document.getElementById('placar-title');
 const placar_content = document.getElementById('placar-content');
-const decricao_placar = document.getElementById('descricao');
 
 async function fetchJogos() {
     const response = await fetch('http://54.233.144.247:3000/jogos');
@@ -9,14 +8,15 @@ async function fetchJogos() {
     jogos.forEach(jogo => {
         const li = document.createElement('li');
         li.id = "placar-list"
-        li.innerHTML = `<div>
-        <p>${jogo.mandante} ${jogo.placar_mandante} X ${jogo.placar_convidado} ${jogo.convidado}</p>
-        <button>Editar</button><button>Remover</button>
-        </div>`;
+        li.innerHTML =
+            `<div>
+                <p>${jogo.mandante} ${jogo.placar_mandante} X ${jogo.placar_convidado} ${jogo.convidado}</p>
+                <button>Editar</button><button>Remover</button>
+            </div>`;
 
         placar_content.appendChild(li);
     });
 };
 
- 
+
 fetchJogos();
