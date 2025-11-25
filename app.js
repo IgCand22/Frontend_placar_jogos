333c440d25e44973988f583907464db6605cf414
IgCand22 <icand32@gmail.com>
2025-11-24 22:24:23 -0300

    Alterações no style e no app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const placar_title = document.getElementById('placar-title');
 const placar_content = document.getElementById('placar-content');
+const decricao_placar = document.getElementById('descricao');
 
 async function fetchJogos() {
-    const response = await fetch('http://54.233.7.139:3000/jogos');
+    const response = await fetch('http://54.233.144.247:3000/jogos');
     const jogos = await response.json();
 
     jogos.forEach(jogo => {
         const li = document.createElement('li');
         li.id = "placar-list"
-        li.innerHTML = `${jogo.mandante} ${jogo.placar_mandante} X ${jogo.convidado} ${jogo.placar_convidado}`;
+        li.innerHTML = `<div>
+        <p>${jogo.mandante} ${jogo.placar_mandante} X ${jogo.placar_convidado} ${jogo.convidado}</p>
+        <button>Editar</button><button>Remover</button>
+        </div>`;
 
         placar_content.appendChild(li);
     });
-
 };
 
  
