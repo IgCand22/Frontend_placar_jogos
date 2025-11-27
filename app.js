5df03f30fb118512b9304241c5e74dceac8b59ff
lgor Candldo <189671858+IgCand22@users.noreply.github.com>
2025-11-27 11:47:34 -0300

    Ultima alteracao

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const ip_public = '54.94.102.200';
 const placar_title = document.getElementById('placar-title');
 const placar_content = document.getElementById('placar-content1');
+const placar_content1 = document.getElementById('placar-content2');
 const btn_procurar = document.getElementById('procurar-btn');
 const criar_jogo = document.getElementById('criar-jogo');
 
-criar_jogo.addEventListener('click', criarJogo);
+criar_jogo.addEventListener('click', criarForm);
 btn_procurar.addEventListener('click', procurarJogo);
 
 async function fetchJogos() {
-    const response = await fetch('http://56.125.224.214:3000/jogos');
+    const response = await fetch(`http://${ip_public}:3000/jogos`);
     const jogos = await response.json();
 
     placar_content.innerHTML = ' '
@@ -26,7 +28,7 @@ async function fetchJogos() {
 };
 
 async function deleteProduct(id) {
-    const response = await fetch('http://56.125.224.214:3000/jogos/' + id, {
+    const response = await fetch(`http://${ip_public}:3000/jogos/` + id, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
@@ -34,12 +36,11 @@ async function deleteProduct(id) {
         //body: JSON.stringify({id})
     });
 
-
-    return response.json();
+    fetchJogos();
 }
 
 // async function editProduct(id) {
-//     const response = await fetch('http://18.228.245.137:3000/jogos/' + id, {
+//     const response = await fetch(`http://${ip_public}:3000/jogos/` + id, {
 //         method: 'PUT',
 //         headers: {
 //             'Content-Type': 'application/json'
@@ -63,7 +64,7 @@ async function procurarJogo() {
 
     let id = parseInt(input.value);
 
-    const response = await fetch(`http://56.125.224.214:3000/jogos/${id}`);
+    const response = await fetch(`http://${ip_public}:3000/jogos/${id}`);
     const jogo = await response.json();
 
     if(jogo.length === 0){
@@ -80,6 +81,53 @@ async function procurarJogo() {
 
 };
 
-async function criarJogo() {
-    alert("deu certo")
+function criarForm(){
+    placar_content1.innerHTML = 
+       `<p>Criar jogo</p>
+         <form id="form-create">
+
+             <label for="mandante">Mandante</label>
+             <input name="mandante" id="mandante" type="text" placeholder="Digite o mandante"></input>
+
+             <label for="convidado-input">Convidado</label>
+             <input name="convidado" id="convidado-input" type="text" placeholder="Digite o convidado"></input>
+         
+             <button type="submit">Criar</button>
+         </form>`
+
+     const form_create = document.getElementById('form-create');
+
+     form_create.addEventListener('submit', function (event) {
+           event.preventDefault();
+           
+           const formData = new FormData(form_create);
+           const data = Object.fromEntries(formData.entries());
+       
+           criarJogo(data);
+
+           placar_content1.innerHTML = ' ';
+           fetchJogos(); 
+     });
+}
+
+async function criarJogo(data) {
+     console.log(data)
+    
+     fetch(`http://${ip_public}:3000/jogos/createJogos`, {
+           method: 'POST',
+           headers: { 'Content-Type': 'application/json' },
+           body: JSON.stringify(data)
+     })
+     .then(response => {
+           if(!response.ok){
+                throw new Error('Erro na requisição: ' + response.statusText);
+           }
+           return response.json();
+     })
+     .then(data => {
+            console.log('Sucesso:', data);
+     })
+     .catch((error) => {
+            console.error('Erro:', error);
+     });      
 }
